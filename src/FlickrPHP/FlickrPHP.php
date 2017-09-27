2be073cd7a3d033ef5c47692129dbfea650e0971
David Nelson <dnelson@lucasoil.com>
2017-09-27 13:15:53 -0700

    Updating composer.json and fixing some bugs

--- src/FlickrPHP/FlickrPHP.php
+++ src/FlickrPHP/FlickrPHP.php
@@ -17,6 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+namespace FlickrPHP;
 if ( !class_exists('FlickrPHP') ) {
 	if (session_id() == "") {
 		@session_start();
